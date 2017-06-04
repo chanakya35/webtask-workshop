f1cc68e057b9e3f3fed7c8be297af016291d1c04
Janak <chanakya35@users.noreply.github.com>
2017-06-04 10:42:15 -0700

    URL inspection

--- wt-1.js
+++ wt-1.js
@@ -1,5 +1,6 @@
 var util = require('util');
 module.exports = function(ctx, cb) {
   console.log(util.inspect(ctx.body, {depth: null}));
+  console.log(util.inspect(ctx.body.issue.url, {depth: null}));
   cb(null, { hello: ctx.data.name || 'Anonymous' });
 };
